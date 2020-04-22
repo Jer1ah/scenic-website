4b07332eb3b52dc89812331527677ecc27035c9b
Jeriah Bowers <jeriah97bowers@yahoo.com>
2020-04-22 19:00:14 -0400

    adding mainPortfolio section

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const serviceListAnimation = (function() {
+//service list item slide up animation
+const serviceListAnimationController = (function() {
     const _serviceList = document.querySelector(".mainServices__list");
     _serviceList.addEventListener("mouseover", () => {
         if(event.target.tagName === "LI") {
@@ -8,6 +9,21 @@ const serviceListAnimation = (function() {
     _serviceList.addEventListener("mouseout", () => {
         if(event.target.tagName === "LI") {
             console.log("out");
+        }
+    });
+}());
+
+
+//portfolio nav active link functionality
+const portfolioActiveLinkController = (function() {
+    const _navigation = document.querySelector("ul.mainPortfolio__navList");
+    const _navigationLinks = document.querySelectorAll(".mainPortfolio__navListItem");
+    _navigation.addEventListener("click", (event) => {
+        if(event.target.tagName === "LI") {
+            _navigationLinks.forEach((item) => {
+                item.classList.remove("active");
+            });
+            event.target.classList.add("active");
         }
     });
 }());
